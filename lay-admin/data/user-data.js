076007108cba9687579d9c55315a5b571ec311cd
liguoxiong <gd.liguoxiong@gmail.com>
2020-02-25 09:49:16 +0800

    add layui table

---
+++ lay-admin/data/user-data.js
@@ -0,0 +1,9 @@
+var users = [
+	{id:1, name:'超级管理员', username:'admin', type:1, lastLogin:'2019-12-29 22:10:03', lock:0},
+	{id:2, name:'张飒', username:'hgieodkk12345', type:0, lastLogin:'2019-12-29 22:10:03', lock:1},
+	{id:3, name:'李四', username:'djfdo4567', type:0, lastLogin:'2019-12-29 22:10:03', lock:0},
+	{id:4, name:'胡一天', username:'huiyee333', type:0, lastLogin:'2019-12-29 22:10:03', lock:0},
+	{id:5, name:'玉湖顾', username:'bietui12', type:0, lastLogin:'2019-12-29 22:10:03', lock:1},
+	{id:6, name:'荣碧玺', username:'gu23yui', type:0, lastLogin:'2019-12-29 22:10:03', lock:0},
+	{id:7, name:'吴谢科', username:'sad0987', type:0, lastLogin:'2019-12-29 22:10:03', lock:0},
+];
